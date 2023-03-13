ed9519def3b49a5e2cbf4a95d6a195874955bc76
cbalesohay <kawipro32@gmail.com>
2023-03-13 13:28:29 -0700

    second

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import Scroll from "./Scroll"
-import "./App.css"
+import "./css/App.css"
 
 
 class App extends Component {
